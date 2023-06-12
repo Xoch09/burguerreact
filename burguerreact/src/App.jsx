4739caa7f348013f53d8896adb62c19df3dd2465
Xochitl <unmananaarchivos@gmail.com>
2023-06-12 16:36:59 -0600

    childrens de buttonRed, ButtonYellow, ButtonLogin

--- burguerreact/src/App.jsx
+++ burguerreact/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ValueBox from './components/ValueBox/ValueBox'
 import './App.css'
+import ButtonRed from './components/ButtonRed/ButtonRed'
+import ButtonYellow from './components/ButtonYellow/ButtonYellow'
+import ButtonLogin from './components/ButtonLogin/ButtonLogin'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,11 +26,19 @@ function App() {
           count is {count}
         </button>
         {/* se llaman children 👈 y el texto se coloca en medio de los dos nombres del componente que sustituyen a un <div></div> */}
-        <ValueBox>email</ValueBox>
-        <ValueBox>contraseña</ValueBox>
+        <ValueBox>ID</ValueBox>
+        <ValueBox>Email</ValueBox>
+        <ValueBox>Rol</ValueBox>
+        {/* botones */}
+        <ButtonRed><button>Eliminar</button></ButtonRed>
+        <ButtonYellow><button>Actualizar</button></ButtonYellow>
+        <ButtonLogin><button>Login</button></ButtonLogin>
+               
         <p>
+          
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
