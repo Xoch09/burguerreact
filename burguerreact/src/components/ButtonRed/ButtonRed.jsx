f60f79200605f77735865df7f2fe82c9a9e6ec0d
Xochitl <unmananaarchivos@gmail.com>
2023-06-12 22:50:53 -0600

    botones, actualizar y eliminar

--- burguerreact/src/components/ButtonRed/ButtonRed.jsx
+++ burguerreact/src/components/ButtonRed/ButtonRed.jsx
@@ -1,13 +1,12 @@
-import "./ButtonRed.css"
+import "./ButtonRed.css";
 
-function ButtonRed ({children}) {
-    return (
-        /*se forman clases para manejarlos mejor en CSS, "nombre del componente + nombre de la clase*/
-        <div className="ButtonRed-container">
-            {/* children que esta en App.jsx para que se muestre el contenido y se dupliquen los bloques, etc */}
-            <p className= "ButtonRed-text">{children}</p>
-        </div>
-    )
+function ButtonRed({ children }) {
+  return (
+    /*se forman clases para manejarlos mejor en CSS, "nombre del componente + nombre de la clase*/
+    <button className="ButtonRed-container">
+      {/* children que esta en App.jsx para que se muestre el contenido y se dupliquen los bloques, etc */}
+      <p className="ButtonRed-text">{children}</p>
+    </button>
+  );
 }
-export default ButtonRed
-
+export default ButtonRed;
--- burguerreact/src/components/ButtonYellow/ButtonYellow.jsx
+++ burguerreact/src/components/ButtonYellow/ButtonYellow.jsx
@@ -1,13 +1,12 @@
-import "./ButtonYellow.css"
+import "./ButtonYellow.css";
 
-function ButtonYellow ({children}) {
-    return (
-        /*se forman clases para manejarlos mejor en CSS, "nombre del componente + nombre de la clase*/
-        <div className="ButtonYellow-container">
-            {/* children que esta en App.jsx para que se muestre el contenido y se dupliquen los bloques, etc */}
-            <p className= "ButtonYellow-text">{children}</p>
-        </div>
-    )
+function ButtonYellow({ children }) {
+  return (
+    /*se forman clases para manejarlos mejor en CSS, "nombre del componente + nombre de la clase*/
+    <button className="ButtonYellow-container">
+      {/* children que esta en App.jsx para que se muestre el contenido y se dupliquen los bloques, etc */}
+      <p className="ButtonYellow-text">{children}</p>
+    </button>
+  );
 }
-export default ButtonYellow
-
+export default ButtonYellow;
