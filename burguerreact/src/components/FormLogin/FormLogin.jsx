1e2c12d54067f95df388dbe0f44779718579d9f6
Xochitl <unmananaarchivos@gmail.com>
2023-06-12 22:51:13 -0600

    form de login

---
+++ burguerreact/src/components/FormLogin/FormLogin.jsx
@@ -0,0 +1,18 @@
+/*Se importa el css, se crea una carpeta*/
+import "./FormLogin.css";
+/*Un componente se usa para reutilizar y dividir, se identifica porque esta escrito con Mayuscula
+y retorna un HTML (JSX)*/
+function FormLogin({ children }) {
+  /*cuando pasan a los parentesis se llaman Props👈*/
+  return (
+    /*se forman clases para manejarlos mejor en CSS, "nombre del componente + nombre de la clase*/
+    <form className="FormLogin-contenedor">
+      <h1 className="Titulo">Burguer Queen</h1>
+      <h2 className="Subtitulo">Bienvenidos</h2>
+      <input id="email" type="text" placeholder="Email" />
+      <input id="password" type="password" placeholder="Password"></input>
+      <button id="buttonLogin" type="submit">Login</button>
+    </form>
+  );
+}
+export default FormLogin;
